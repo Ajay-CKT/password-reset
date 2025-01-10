d08695dfc39214b05f53edf8b616b4dc5671b5d4
Ajay-CKT <ajayckt917@gmail.com>
2025-01-10 19:45:45 +0530

    some random changes

--- server/app.js
+++ server/app.js
@@ -8,28 +8,47 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(
-//   cors({
-//     origin: CLIENT_URL,
-//     credentials: true,
-//   })
-// );
-
 app.use(
   cors({
     origin: CLIENT_URL,
     credentials: true,
-    allowedHeaders: [
-      "Origin",
-      "Content-Type",
-      "Accept",
-      "Authorization",
-      "X-Request-With",
-    ],
   })
 );
 
+/*
+
+const allowedOrigins = [CLIENT_URL];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.use((request, response, next) => {
+  response.setHeader("Access-Control-Allow-Origin", CLIENT_URL);
+  response.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.options("*", cors());
+
+*/
 
 app.use(cookieParser());
 
